aae0d098710d74cbc830a9b4332e1d042a8ab7a6
Tony Pham <thanh01.pmt@gmail.com>
2021-04-24 10:37:54 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 I2C_LCD1602.LcdInit(63)
-I2C_LCD1602.ShowString("Smart Pot", 0, 0)
-I2C_LCD1602.ShowString("Hello......", 0, 1)
+I2C_LCD1602.ShowString("--- SMART POT --", 0, 0)
+I2C_LCD1602.ShowString("      HELLO     ", 0, 1)
 basic.pause(2000)
-I2C_LCD1602.ShowString("POT  | Temp:   C", 0, 0)
+I2C_LCD1602.ShowString(" POT | Tem:    C", 0, 0)
 I2C_LCD1602.ShowString("INFO | Hum:    %", 0, 1)
 basic.forever(function () {
     dht11_dht22.queryData(
@@ -12,6 +12,6 @@ basic.forever(function () {
     true,
     true
     )
-    I2C_LCD1602.ShowNumber(Math.round(dht11_dht22.readData(dataType.temperature)), 13, 0)
-    I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.humidity), 13, 1)
+    I2C_LCD1602.ShowNumber(Math.round(dht11_dht22.readData(dataType.temperature)), 12, 0)
+    I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.humidity), 12, 1)
 })
